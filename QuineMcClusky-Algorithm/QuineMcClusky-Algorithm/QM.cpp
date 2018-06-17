cebd8bbd529f9cf227503d6939bc3251684479fc
Maaaaaatin <martin-oehme@gmx.de>
2018-06-17 19:30:09 +0200

    Fully implemented QM-Class

--- QuineMcClusky-Algorithm/QuineMcClusky-Algorithm/QM.cpp
+++ QuineMcClusky-Algorithm/QuineMcClusky-Algorithm/QM.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "QM.h"
 
-
+#define MAX_VARIABLES 20
 QM::QM(int numberElements)
 {
 	VARIABLES = numberElements;
@@ -11,14 +11,133 @@ QM::QM(int numberElements)
 vector<string> QM::getVars()
 {
 	vector<string> v;
-	char* letters[20];
+	string letters[MAX_VARIABLES];
 	int i = 0;
-	while (i < strlen(*letters))
+
+	while (i < MAX_VARIABLES)
 	{
-		strcat(letters[i], strcat("x", i.ToString));
+		letters[i] = "x" + to_string(i);
 	}
 	for (int i = 0; i<this->VARIABLES; i++)
 		v.push_back(letters[i]);
 
 	return v;
+}
+
+string QM::decToBin(int number)
+{
+	if (number == 0 || number == 1)
+		return to_string(number) + "";
+
+	if (number % 2 == 0)
+		return decToBin(number / 2) + "0";
+	else
+		return decToBin(number / 2) + "1";
+}
+
+string QM::zeroes(string binStr)
+{
+	int max = VARIABLES - binStr.length();
+	for (int i = 0; i<max; i++)
+		binStr = "0" + binStr;
+	return binStr;
+}
+
+bool QM::isGreyCode(string a, string b)
+{
+	int flag = 0;
+	for (unsigned int i = 0; i<a.length(); i++)
+	{
+		if (a[i] != b[i])
+			flag++;
+	}
+	return (flag == 1);
+}
+
+
+string QM::makeDontcares(string a, string b)
+{
+	string temp = "";
+	for (unsigned int i = 0; i < a.length(); i++)
+	{
+		if (a[i] != b[i])
+			temp = temp + "-";
+		else
+			temp = temp + a[i];
+	}
+	return temp;
+}
+
+bool QM::isInVector(vector<string> a, string b)
+{
+	for (unsigned int i = 0; i<a.size(); i++)
+		if (a[i].compare(b) == 0)
+			return true;
+	return false;
+}
+
+vector<string> QM::reduce(vector<string> toReduce)
+{
+	vector<string> reduced;
+
+	int max = toReduce.size();
+	int* checked = (int*) malloc(max * sizeof(int));
+	for (int i = 0; i<max; i++)
+	{
+		for (int j = i; j<max; j++)
+		{
+			//alle Elemente auf Grey-Code überprüfen, ggf. dontcares einfügen
+			if (isGreyCode(toReduce[i], toReduce[j]))
+			{
+				checked[i] = 1;
+				checked[j] = 1;
+				if (!isInVector(reduced, makeDontcares(toReduce[i], toReduce[j])))
+					reduced.push_back(makeDontcares(toReduce[i], toReduce[j]));
+			}
+		}
+	}
+
+	//übrige Elemente in neuen Vektor schreiben und zurück geben
+	for (int i = 0; i<max; i++)
+	{
+		if (checked[i] != 1 && !isInVector(reduced, toReduce[i]))
+			reduced.push_back(toReduce[i]);
+	}
+
+	return reduced;
+}
+
+string QM::toCharacter(string boolStr)
+{
+	string temp = "";
+	vector<string> vars = this->getVars();
+	if (boolStr.compare(*dontcares) == 0)
+		return "1";
+
+	for (unsigned int i = 0; i<boolStr.length(); i++)
+	{
+		if (boolStr[i] != '-')
+		{
+			if (boolStr[i] == '0')
+				temp = temp + vars[i] + "\'";
+			else
+				temp = temp + vars[i];
+		}
+	}
+	return temp;
+}
+
+bool QM::EqualVectors(vector<string> a, vector<string> b)
+{
+	if (a.size() != b.size())
+		return false;
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	for (unsigned int i = 0; i<a.size(); i++)
+	{
+		if (a[i] != b[i])
+			return false;
+	}
+	return true;
 }
--- QuineMcClusky-Algorithm/QuineMcClusky-Algorithm/QM.h
+++ QuineMcClusky-Algorithm/QuineMcClusky-Algorithm/QM.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,7 +10,24 @@ ref class QM
 {
 public:
 	QM(int numberElements);
+	//um die boolean Variablen zu erhalten
 	vector<string> getVars();
+	//wandelt dezimal zahlen in binär
+	string decToBin(int number);
+	//Nullen auffüllen
+	string zeroes(string binStr);
+	//auf Grey-Code überprüfen
+	bool isGreyCode(string a, string b);
+	//dontcares einfügen
+	string makeDontcares(string a, string b);
+	//check ob b schon in Vektor a vorhanden ist
+	bool isInVector(vector<string> a, string b);
+	//gleichung reduzieren
+	vector<string> reduce(vector<string> toReduce);
+	//bool'sche ausdrücke in Buchstaben umwandeln
+	string toCharacter(string boolStr);
+	//check ob 2 Vektoren gleich sind
+	bool EqualVectors(vector<string> a, vector<string> b);
 private:
 	int VARIABLES;
 	string* dontcares;
